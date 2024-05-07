5e159309e2e919a691bc4a16bf3ee5e896c35111
Shakil53 <shakilahmed.dcc@gmail.com>
2024-05-08 00:54:05 +0600

    card header item edited done

--- src/components/header/CardHeaderItem.tsx
+++ src/components/header/CardHeaderItem.tsx
@@ -5,12 +5,12 @@ import { FaHeart } from "react-icons/fa6";
 const CardHeaderItem = () => {
     return (
 
-<div className='flex w-full max-w-[1280px] mx-auto mb-8 flex-col md:flex-row items-center mt-6 space-y-4 md:space-y-0 gap-2 md:gap-10'>
+<div className='flex w-full max-w-[1280px] mx-auto mb-8 mt-4 md:-mt-16 flex-col md:flex-row items-center space-y-4 md:space-y-0 gap-2 md:gap-10'>
              <div className=''>
             <Card className="w-56 bg-opacity-50 backdrop-filter backdrop-blur-sm rounded-3xl">
       
         <CardContent className='p-5 '>
-                    <div className='rounded-3xl w-[48px] h-[48px] bg-[#685FD4] opacity-35 flex justify-center items-center'>
+                    <div className='rounded-3xl w-[48px] h-[48px] bg-[#ffb443] opacity-35 flex justify-center items-center'>
                         <div className=''>
                         <FaHeart />
                         </div>
@@ -24,7 +24,7 @@ const CardHeaderItem = () => {
             <Card className="w-56 bg-opacity-50 backdrop-filter backdrop-blur-sm rounded-3xl">
       
         <CardContent className='p-5 '>
-                    <div className='rounded-3xl w-[48px] h-[48px] bg-[#685FD4] opacity-35 flex justify-center items-center'>
+                    <div className='rounded-3xl w-[48px] h-[48px] bg-[#6dd45f] opacity-35 flex justify-center items-center'>
                         <div className=''>
                         <FaHeart />
                         </div>
