7ae07688faa05f68bdce0212ced9e478589d170f
Shakil53 <shakilahmed.dcc@gmail.com>
2024-05-26 19:18:46 +0600

    rearrange enventitems and gallery

--- src/components/EventItem/EventItems.tsx
+++ src/components/EventItem/EventItems.tsx
@@ -1,43 +1,24 @@
-import img1 from '../../assets/event/1.png'
-import img2 from '../../assets/event/2.png'
-import img3 from '../../assets/event/3.png'
-import img4 from '../../assets/event/4.png';
-import img5 from '../../assets/event/5.png';
-import img6 from '../../assets/event/6.png';
+import img from '../../assets/event/Frame 1120.png'
+
 
 
 const EventItems = () => {
-    const images = [
-        { id: 1, src: img1, alt: 'Event Item - 1' },
-        { id: 2, src: img2, alt: 'Event Item - 2' },
-        { id: 3, src: img3, alt: 'Event Item - 3' },
-        { id: 4, src: img4, alt: 'Event Item - 4' },
-        { id: 5, src: img5, alt: 'Event Item - 5' },
-        { id: 6, src: img6, alt: 'Event Item - 6' },
-    ];
-    
+   
    
       
     
       
     return (
-        <div className='bg-gradient-to-tl from-[#6FCF97] to-pink-300'>
+        <div className='flex bg-gradient-to-tl from-[#6FCF97] to-pink-300'>
             <div className='bg-white bg-opacity-80 backdrop-contrast-125 backdrop-filter backdrop-blur-3xl p-4 shadow-lg'>
             <div>
         <h1 className="text-2xl md:text-6xl font-bold text-center mt-10">Event Items</h1>
             <p className="text-center mt-4 ">Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.</p>
     </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 bg-gray-200/50 p-5">
-        {images.map((image) => (
-          <div key={image.id} className="border rounded-lg overflow-hidden shadow-lg">
-            <img src={image.src} alt={image.alt} className={`w-full object-cover h-40 `} />
-            <div className="p-4 text-center">
-              <p className="font-semibold">{image.alt}</p>
-            </div>
-          </div>
-        ))}
+          <div className='mt-10'>
+            
+           <img  src={img}></img>
     </div>
-           
             </div> 
     </div>
     );
--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,38 +1,17 @@
 import { CircleCheck,  } from "lucide-react";
-import img1 from '../../assets/gallery/Picture (1).png'
-import img2 from '../../assets/gallery/Picture (2).png'
-import img3 from '../../assets/gallery/Picture (3).png'
-import img4 from '../../assets/gallery/Picture (4).png'
-import img5 from '../../assets/gallery/Picture (5).png'
-import img6 from '../../assets/gallery/Picture (6).png'
-import img7 from '../../assets/gallery/Picture (7).png'
-import img8 from '../../assets/gallery/Picture (8).png'
-import img9 from '../../assets/gallery/Picture (9).png'
-import img10 from '../../assets/gallery/Picture.png'
-import img11 from '../../assets/gallery/Star.png'
+import img from '../../assets/gallery/Col.png'
+
 import Container from "../Container";
 
 
 
 const Gallery = () => {
-    const images = [
-        { id: 1, src: img11, alt: 'Event Item - 1' },
-        { id: 2, src: img2, alt: 'Event Item - 2' },
-        { id: 3, src: img3, alt: 'Event Item - 3' },
-        { id: 4, src: img4, alt: 'Event Item - 4' },
-        { id: 5, src: img5, alt: 'Event Item - 5' },
-        { id: 6, src: img6, alt: 'Event Item - 6' },
-        { id: 7, src: img7, alt: 'Event Item - 7' },
-        { id: 8, src: img8, alt: 'Event Item - 8' },
-        { id: 9, src: img9, alt: 'Event Item - 9' },
-        { id: 10, src: img10, alt: 'Event Item - 10' },
-        { id: 11, src: img1, alt: 'Event Item - 11' },
-    ];
+    
     
     return (
         <Container className="">
-             <div  className="p-4 mt-6">
-            <div className="flex gap-24">
+             <div  className="p-4 mt-6 justify-center items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-2 ">
                  {/* gallery content */}
                     <div>
                     <div className="space-y-3">
@@ -42,7 +21,7 @@ const Gallery = () => {
                {/* gallery items */}
             <div className="w-full mx-auto flex flex-col md:flex-row md:justify-between items-center md:items-center mt-6 space-y-4 md:space-y-0">
                  
-                    <div className="w-[400px] h-[648px] rounded-lg border-t-4 border-green-500 bg-white">
+                    <div className="w-[400px] h-[248px] rounded-lg border-t-4 border-green-500 bg-white">
 
                        
                         <div className="p-4">
@@ -78,14 +57,10 @@ const Gallery = () => {
                 </div>
             </div>
                 {/* gallery image */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {images.map((photo, index) => (
-                <div key={index} className="overflow-hidden rounded-lg shadow-lg">
-                <img src={photo.src} alt={`Gallery Photo ${index + 1}`} className="w-full h-full object-cover" />
+                    <div className='mt-10'>
+                            <img  src={img}></img>
+                    </div>
             </div>
-             ))}
-         </div>
-           </div>
         </div>
        </Container>
     );
