7395811dce25a04fbd8527d467e66df7a4761b22
Shakil53 <shakilahmed.dcc@gmail.com>
2024-05-26 18:49:56 +0600

    gallery photo push

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,13 +1,41 @@
 import { CircleCheck,  } from "lucide-react";
+import img1 from '../../assets/gallery/Picture (1).png'
+import img2 from '../../assets/gallery/Picture (2).png'
+import img3 from '../../assets/gallery/Picture (3).png'
+import img4 from '../../assets/gallery/Picture (4).png'
+import img5 from '../../assets/gallery/Picture (5).png'
+import img6 from '../../assets/gallery/Picture (6).png'
+import img7 from '../../assets/gallery/Picture (7).png'
+import img8 from '../../assets/gallery/Picture (8).png'
+import img9 from '../../assets/gallery/Picture (9).png'
+import img10 from '../../assets/gallery/Picture.png'
+import img11 from '../../assets/gallery/Star.png'
+import Container from "../Container";
 
 
 
 const Gallery = () => {
+    const images = [
+        { id: 1, src: img11, alt: 'Event Item - 1' },
+        { id: 2, src: img2, alt: 'Event Item - 2' },
+        { id: 3, src: img3, alt: 'Event Item - 3' },
+        { id: 4, src: img4, alt: 'Event Item - 4' },
+        { id: 5, src: img5, alt: 'Event Item - 5' },
+        { id: 6, src: img6, alt: 'Event Item - 6' },
+        { id: 7, src: img7, alt: 'Event Item - 7' },
+        { id: 8, src: img8, alt: 'Event Item - 8' },
+        { id: 9, src: img9, alt: 'Event Item - 9' },
+        { id: 10, src: img10, alt: 'Event Item - 10' },
+        { id: 11, src: img1, alt: 'Event Item - 11' },
+    ];
+    
     return (
-        <div  className="p-4 mt-6">
-            <div>
+        <Container className="">
+             <div  className="p-4 mt-6">
+            <div className="flex gap-24">
                  {/* gallery content */}
-            <div className="space-y-3">
+                    <div>
+                    <div className="space-y-3">
                 <h1 className="text-4xl font-bold">Gallery</h1>
                 <p className="max-w-md">Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.</p>
             </div>
@@ -48,12 +76,18 @@ const Gallery = () => {
                    
                    
                 </div>
+            </div>
                 {/* gallery image */}
-                <div>
-
-                </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {images.map((photo, index) => (
+                <div key={index} className="overflow-hidden rounded-lg shadow-lg">
+                <img src={photo.src} alt={`Gallery Photo ${index + 1}`} className="w-full h-full object-cover" />
+            </div>
+             ))}
+         </div>
            </div>
         </div>
+       </Container>
     );
 };
 
